52997fe19605e4d908e66d3ef653e98c39eb0116
Cyboot <Cyboot@timweb.de>
2015-02-11 23:02:08 +0100

    update for maven

--- server/src/main/java/de/timweb/blueocean/sitecreator/StatsSite.java
+++ server/src/main/java/de/timweb/blueocean/sitecreator/StatsSite.java
@@ -12,7 +12,7 @@ public class StatsSite implements Site {
 
 	@Override
 	public String getHTML(Request request) throws IOException {
-		return FileUtils.readFileToString(new File("web/stats.html")) + "äüö";
+		return FileUtils.readFileToString(new File("web/stats.html")) + "Ã¶Ã¤Ã¼";
 	}
 
 }
